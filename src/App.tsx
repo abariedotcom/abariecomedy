11083bbc3e3881d6cbac7b6812c9f97cd7b0219e
abariedotcom <kim.b.biv@gmail.com>
2026-06-12 21:51:30 +0100

    Add Events route to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Events from './pages/Events';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
       </Routes>
     </BrowserRouter>
   );

c27a905f063b4e8696660effbf555a0e607cf571
abariedotcom <kim.b.biv@gmail.com>
2026-06-13 13:43:27 +0100

    Add Tickets route to App router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Events from './pages/Events';
 import Portfolio from './pages/Portfolio';
+import Tickets from './pages/Tickets';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/events" element={<Events />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/tickets" element={<Tickets />} />
       </Routes>
     </BrowserRouter>
   );
